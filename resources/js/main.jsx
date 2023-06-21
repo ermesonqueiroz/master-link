a85996619f69245fde8c70f07ec1cda41c9fcc3d
Ermeson Sampaio <ermeson.s.queiroz@gmail.com>
2023-06-21 19:39:05 -0300

    feat: temporary landing page

--- resources/js/main.jsx
+++ resources/js/main.jsx
@@ -2,6 +2,9 @@ import './bootstrap';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import "@fontsource/dm-sans";
+import "@fontsource/dm-sans/500.css";
+import "@fontsource/dm-sans/700.css";
 import '../css/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
