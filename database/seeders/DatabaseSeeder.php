3e3ee06f5e59dc27de6b65c18d80109d8980436a
Ermeson Sampaio <ermeson.s.queiroz@gmail.com>
2023-06-27 17:19:53 -0300

    reset project to initial state of laravel

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,20 +2,21 @@
 
 namespace Database\Seeders;
 
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
+    /**
+     * Seed the application's database.
+     */
     public function run(): void
     {
-         User::factory(5)->create();
+        // \App\Models\User::factory(10)->create();
 
-         User::factory()->create([
-             'username' => 'admin',
-             'display_name' => 'Admin',
-             'email' => 'admin@admin.com',
-             'password' => password_hash('admin123', PASSWORD_DEFAULT)
-         ]);
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/{path?}',"welcome")->where("path", ".*");
+Route::get('/', function () {
+    return view('welcome');
+});
