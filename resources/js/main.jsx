cdedd526d2178ba3eb78b65cc6f755a53f0e0053
Ermeson Sampaio <ermeson.s.queiroz@gmail.com>
2023-06-28 13:11:59 -0300

    feat: react setup

---
+++ resources/js/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './bootstrap';
+import ReactDOM from 'react-dom/client';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <h1>Hello World</h1>
+    </React.StrictMode>
+);
