23a71fe3968b620c53c69ea758d032f1a3c05887
Ermeson Sampaio <ermeson.s.queiroz@gmail.com>
2023-07-01 12:57:17 -0300

    feat: add favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>unilink</title>
+    <link rel="icon" type="image/svg" href="/favicon.svg">
     @vite('resources/css/globals.css')
 </head>
 
